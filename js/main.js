a741f5649f900780f9cb4d4e67528be3aa64457b
Vyacheslav-Bespalov <39215013+Vyacheslav-Bespalov@users.noreply.github.com>
2020-11-16 00:53:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    const form = document.querySelector('.form');
+    const formSubmit = form.querySelector('.form__submit');
+
+
+    const checkForm = () => {
+        if (form.checkValidity()) {
+            console.log('valid');
+            formSubmit.removeAttribute('disabled', 'disabled');
+        } else {
+            formSubmit.setAttribute('disabled', 'disabled');
+        }
+    }
+
+    let timeout;
+    const checkTimeout = () => {
+        if (timeout) {
+            window.clearTimeout(timeout);
+        }
+        timeout = window.setTimeout(checkForm, 500);
+    }
+
+    form.addEventListener('input', checkTimeout);
+});
+
